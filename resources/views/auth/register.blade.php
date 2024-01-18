f4a55280b0d34f4899f6c7584bca168009c41e6b
C0SMIC-ONE <vhon23bautista@gmail.com>
2024-01-18 10:06:47 +0800

    last

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -109,7 +109,7 @@
                         <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required>
                     </div>
                     <div class="ml-3 text-sm">
-                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">{{ __('I accept the') }} <a class="font-medium text-blue-600 hover:underline dark:text-blue-500" href="#">{{ __('Terms and Conditions') }}</a></label>
+                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">{{ __('I accept the') }} <a class="font-medium text-blue-600 hover:underline dark:text-blue-500" href="{{ asset('assets/terms/Terms-and-Conditions-for-PSUIMMS.docx') }}">{{ __('Terms and Conditions') }}</a></label>
                     </div>
                 </div>
     
