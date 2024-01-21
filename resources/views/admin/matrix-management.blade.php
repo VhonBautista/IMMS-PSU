d6fae59b2066f6d77170bbc4d513c09f320e3809
C0SMIC-ONE <vhon23bautista@gmail.com>
2024-01-21 11:58:27 +0800

    last na to naayos na lahat 2

--- resources/views/admin/matrix-management.blade.php
+++ resources/views/admin/matrix-management.blade.php
@@ -223,7 +223,7 @@
                                                                     <li>
                                                                         <div class="flex w-full justify-between items-center text-sm mb-1">
                                                                             <div class="flex items-center gap-1">
-                                                                                <span class="text-gray-600 font-normal dark:text-gray-500">{{ $matrixItem->item . ' (' . $matrixItem->score . ' points)' }}</span>
+                                                                                <span class="text-gray-600 font-normal dark:text-gray-500">{{ $matrixItem->item . ' (' . $matrixItem->score . '%)' }}</span>
                             
                                                                                 <svg class="w-3 h-3 text-gray-800 dark:text-white" data-tooltip-placement="bottom" data-tooltip-target="tooltip-text-{{ $matrix->id . '-' . $subMatrix->id . '-' . $matrixItem->id }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                                                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9h2v5m-2 0h4M9.408 5.5h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
